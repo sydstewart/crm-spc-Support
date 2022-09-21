3a3c9abd98aeb02d5cf66249601f5b4dbc65ed26
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-09-21 15:58:54 +0000

    Created Form1.RowTemplate1; Edited settings, ServerModule1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -10,5 +10,12 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+    
+    self.repeating_panel_1.items = anvil.server.call('get_Sales_Existing_and_New')
+
+  def plot_1_click(self, points, **event_args):
+    """This method is called when a data point is clicked."""
+    pass
+
 
 
--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,23 +1,16 @@
+import anvil.secrets
 import anvil.server
-
+import pymysql
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
 #
-# To allow anvil.server.call() to call functions here, we mark
-# them with @anvil.server.callable.
-# Here is an example - you can replace it with your own:
-#
-# @anvil.server.callable
-# def say_hello(name):
-#   print("Hello, " + name + "!")
-#   return 42
-#
+
 
 def connect():
   connection = pymysql.connect(host='51.141.236.29',
                                port=3306,
                                user='CRMReadOnly',
-                               password=anvil.secrets.get_secret('CrM'),
+                               password=anvil.secrets.get_secret('CRM'),
                                database = 'infoathand',
                                cursorclass=pymysql.cursors.DictCursor)
   return connection
@@ -27,16 +20,16 @@ def get_Sales_Existing_and_New():
   conn = connect()
   with conn.cursor() as cur:
     cur.execute(
-"Select Date_Format(invoice.date_entered, '%01/%m/%Y') As YM \
-      , Sum(invoice.amount_usdollar) As \
-       NewandExisting_Invoice_total \
-From invoice Inner Join \
-  invoice_cstm On invoice_cstm.id_c = invoice.id Inner Join \
-  accounts On invoice.shipping_account_id = accounts.id \
-Where (invoice_cstm.newexistingormaintenance_c = 'New') Or \
-  (invoice_cstm.newexistingormaintenance_c = 'Existing') \
-Group By YM \
-Order By YM")  
+                "Select Date_Format(invoice.date_entered, '%01/%m/%Y') As YM \
+                      , Sum(invoice.amount_usdollar) As \
+                      NewandExisting_Invoice_total \
+                From invoice Inner Join \
+                  invoice_cstm On invoice_cstm.id_c = invoice.id Inner Join \
+                  accounts On invoice.shipping_account_id = accounts.id \
+                Where (invoice_cstm.newexistingormaintenance_c = 'New') Or \
+                  (invoice_cstm.newexistingormaintenance_c = 'Existing') \
+                Group By YM \
+                Order By YM")  
     
     return cur.fetchall()
 
