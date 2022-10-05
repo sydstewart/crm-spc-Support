8472ab85457c4b3619430ae2dcdc4a8cce829997
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-10-05 16:56:21 +0000

    Edited OutofControlChecks

--- server_code/OutofControlChecks.py
+++ server_code/OutofControlChecks.py
@@ -242,7 +242,7 @@ def outofcontrol6fall(df, pointdate, pointname, total_rows, pointmean, sd ):
   print()
 
   outofcontrol6fall = pd.DataFrame() 
-  for i in range(5,total_rows):
+  for i in range(6,total_rows):
       countx = 0
       if (df[pointname].iloc[i]  < df[pointname].iloc[i-1]):
               countx = 1
@@ -258,10 +258,10 @@ def outofcontrol6fall(df, pointdate, pointname, total_rows, pointmean, sd ):
 #               print(df[pointname].iloc[i-3], i-3, countx)
       if (df[pointname].iloc[i-4]  < df[pointname].iloc[i - 5]):
               countx = countx + 1
-#               print(df[pointname].iloc[i-4], i-4, countx)
+              print(df[pointname].iloc[i-4], i-4, countx)
       if (df[pointname].iloc[i - 5]  < df[pointname].iloc[i-6] ):
               countx = countx + 1
-#               print(df[pointname].iloc[i-5], i-5, countx)
+              print(df[pointname].iloc[i-5], i-5, countx)
       if (df[pointname].iloc[i - 6]  < df[pointname].iloc[i-7] ):
                        countx = countx + 1
       print(countx)
@@ -273,7 +273,7 @@ def outofcontrol6fall(df, pointdate, pointname, total_rows, pointmean, sd ):
               outofcontrol6fall = outofcontrol6fall.append({pointdate: df[pointdate].iloc[i-1],pointname:df[pointname].iloc[i-1]}, ignore_index=True)
               outofcontrol6fall = outofcontrol6fall.append({pointdate: df[pointdate].iloc[i],pointname:df[pointname].iloc[i]}, ignore_index=True)
               countx = 0
-#       print('outofcontrol6fall',outofcontrol6fall)
+  print('outofcontrol6fall',outofcontrol6fall)
       
   if outofcontrol6fall.empty:
         down6 = go.Scatter(
