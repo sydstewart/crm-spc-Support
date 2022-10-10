7d8ed78fec31766699202d1eb84357120b36edc3
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-10-10 14:22:29 +0000

    Edited Cases_Waiting_on_4S_from_Table.RowTemplate4, Cases_Waiting_on_4S_from_Table, settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -86,7 +86,7 @@ def get_Waiting_on_4S(tablename,columnname, startdate, enddate, showmeans):
                                               )
   
     total_rows = len(waitinglist)  
-    dicts = [{'Date_Entered': r['Date_Entered'],columnname: r[columnname]}
+    dicts = [{'Date_Entered': r['Date_Entered'],columnname: r[columnname],'NoteCol':r['noteCol']}
             for r in waitinglist]
 #     print (dicts)
 
@@ -96,7 +96,7 @@ def get_Waiting_on_4S(tablename,columnname, startdate, enddate, showmeans):
     df['Mean']= df[columnname].mean()
     pointdate= 'Date_Entered'
     pointname = columnname
-    
+    noteCol = df['NoteCol']
     Mean = df[columnname].mean()
     SD = df[columnname].std()
     total_rows = total_rows = df[columnname].count() - 1
@@ -116,6 +116,9 @@ def get_Waiting_on_4S(tablename,columnname, startdate, enddate, showmeans):
                         y = df[columnname],
                         mode ='markers + lines',
                         name= columnname),
+    go.Scatter(          x = df['Date_Entered'], 
+                         y = df[columnname], mode='text',
+                         name='Annotations', text=df[noteCol], textposition='top left'),
     go.Scatter(
                         x=df['Date_Entered'],
                         y = df['Mean'] ,
