81a769a27d05bb86818b3dc6f75e5a1926172db5
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-11-25 16:20:54 +0000

    Created Teamwork, Projects.RowTemplate12 and Projects; Edited settings

---
+++ server_code/Teamwork.py
@@ -0,0 +1,41 @@
+import anvil.email
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.secrets
+import anvil.server
+import pymysql
+
+def connect():
+  connection = pymysql.connect(host='51.141.236.29',
+                               port=3306,
+                               user='CRMReadOnly',
+                               password=anvil.secrets.get_secret('CRM'),
+                               database = 'teamwork',
+                               cursorclass=pymysql.cursors.DictCursor)
+  return connection
+
+@anvil.server.callable
+def listprojects():
+  conn = connect()
+  with conn.cursor() as cur:
+    cur.execute(
+#                 "Select Date_Format(invoice.date_entered, '%Y/%m/%01') As YM \
+#                       , Sum(invoice.amount_usdollar) As \
+#                       NewandExisting_Invoice_total \
+#                 From invoice Inner Join \
+#                   invoice_cstm On invoice_cstm.id_c = invoice.id Inner Join \
+#                   accounts On invoice.shipping_account_id = accounts.id \
+#                 Where (invoice_cstm.newexistingormaintenance_c = 'New') Or \
+#                   (invoice_cstm.newexistingormaintenance_c = 'Existing') \
+#                 Group By YM \
+#                 Order By Date_Format(Date(invoice.date_entered), '%Y/%m')")  
+                  "Select * from projects")
+    
+#     return cur.fetchall() 
+  dicts = [{'Name': r['projectname'],'StartDate':r['startDate']}
+            for r in cur.fetchall()]
+  return dicts
