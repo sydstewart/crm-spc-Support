4e3e652b635769c3c3d1d96f2fcd2e0f7f58ae79
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-12-09 11:33:01 +0000

    Edited settings and Projects

--- client_code/Projects/__init__.py
+++ client_code/Projects/__init__.py
@@ -11,12 +11,20 @@ class Projects(ProjectsTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    
     # Any code you write here will run when the form opens.
 #     projects, total_rows = anvil.server.call('listprojects')
-    projectslist = app_tables.projects.search(boardname=self.multi_select_drop_down_1.items)
+    projectslist = app_tables.projects.search()
     
     print (projects)
     print('Total Rows = ', total_rows)
     self.total_rows_text.text = total_rows
-    self.repeating_panel_1.items = projects
+    self.repeating_panel_1.items = projectslist
+
+  def multi_select_drop_down_1_change(self, **event_args):
+    """This method is called when the selected values change"""
+    selectedlist = self.multi_select_drop_down_1.selected
+    projectslist = app_tables.projects.search(q.any_of(boardname=(*selectedlist)))
+    self.repeating_panel_1.items = projectslist
+    pass
+
